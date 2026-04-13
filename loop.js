8ffc95d01dfd69afc6a404511906038d3a1f46aa
Dhilip <dhiliban.m@payhuddle.com>
2026-04-13 14:38:58 +0530

    while loop added

--- loop.js
+++ loop.js
@@ -14,3 +14,13 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
 
 }
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log('conflict rebase')
+
+}
+
+while (condition) {
+
+}
