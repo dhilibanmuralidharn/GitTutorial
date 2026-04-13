a0074ca95f12ce2213621f74bc23cccf7a12a437
Dhilip <dhiliban.m@payhuddle.com>
2026-04-13 13:03:21 +0530

    bug-fixed

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ for (let index = 0; index < array.length; index++) {
     const eleent = arra[index]
     console.log(eleent)
 }
+// bug sloved. added extra loop
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+}
