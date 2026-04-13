cbe2d7567405a0ffb5da4ddc9a147c6fb8c7e5c0
Dhilip <dhiliban.m@payhuddle.com>
2026-04-13 14:24:20 +0530

    stash-changeinto- finishing

---
+++ loop.js
@@ -0,0 +1,16 @@
+// this file created for stash changes
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+}
--- stash.js
+++
@@ -1,5 +0,0 @@
-// this file created for stash changes
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-
-}
