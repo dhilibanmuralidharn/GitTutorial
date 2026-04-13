460383161f4edd8c11fe387b3789d6f409f3951c
Dhilip <dhiliban.m@payhuddle.com>
2026-04-13 14:42:48 +0530

    update the loop js and css

--- loop.js
+++ loop.js
@@ -14,3 +14,13 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
 
 }
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log('conflict solved')
+}
+
+while (condition) {
+
+}
