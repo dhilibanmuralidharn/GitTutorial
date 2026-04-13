be1038f742754aa5a52719d75ae8a8ca29fb5185
Dhilip <dhiliban.m@payhuddle.com>
2026-04-13 14:26:48 +0530

    update index.js

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
 
 }
+
+// update the feature one 1
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log('object')
+
+}
+
+// update the another loop 
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+}
